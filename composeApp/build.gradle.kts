c04912611df3c2d37194dff6ddc590428bf4fca6
harutiro <harutiro2027@outlook.jp>
2024-07-21 20:25:19 +0900

    add: gradleの更新を行なった

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -52,6 +52,7 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation (libs.play.services.instantapps)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
